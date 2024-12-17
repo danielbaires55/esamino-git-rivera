1ac944827059310be5afcd65162e1a7c287c380d
root <root@DESKTOP-KQRALU3>
2024-12-17 14:18:13 +0100

    feat: aggiunta funzione addTodo

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log('Todo App');
+
+let todos = [];
+let nextId = 1;
+
+function addTodo(title, category) {
+    const todo = {
+        id: nextId++,
+        title,
+        category,
+        completed: false
+    };
+    todos.push(todo);
+    console.log('Todo aggiunto:', todo);
+}
+
+function deleteTodo(id) {
+    todos = todos.filter(todo => todo.id !== id);
+    console.log('Todo eliminato:', id);
+}
+
+function toggleComplete(id) {
+    const todo = todos.find(todo => todo.id === id);
+    if (todo) {
+        todo.completed = !todo.completed;
+        console.log('Todo aggiornato:', todo);
+    }
+}
