e004706aec69772022d89c22800f6c7ce641384c
root <root@DESKTOP-KQRALU3>
2024-12-17 14:28:05 +0100

    feat: persistenza Todo con localStorage

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-console.log('Todo App');
-
 let todos = [];
 let nextId = 1;
+
+function loadTodos() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+        todos = JSON.parse(storedTodos);
+        nextId = todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    }
+}
+
+
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
 
 function addTodo(title, category) {
     const todo = {
@@ -11,26 +22,26 @@ function addTodo(title, category) {
         completed: false
     };
     todos.push(todo);
+    saveTodos();
     console.log('Todo aggiunto:', todo);
 }
 
+
 function deleteTodo(id) {
     todos = todos.filter(todo => todo.id !== id);
+    saveTodos();
     console.log('Todo eliminato:', id);
 }
+
 
 function toggleComplete(id) {
     const todo = todos.find(todo => todo.id === id);
     if (todo) {
         todo.completed = !todo.completed;
+        saveTodos();
         console.log('Todo aggiornato:', todo);
     }
 }
 
-function filterTodos(status, category) {
-    return todos.filter(todo => {
-        const statusMatch = status === 'all' || (status === 'completed' && todo.completed) || (status === 'not-completed' && !todo.completed);
-        const categoryMatch = category === 'all' || todo.category === category;
-        return statusMatch && categoryMatch;
-    });
-}
+
+loadTodos();
